0da0e36967e7c6f23f73a60faeaa918176da615e
djamelall22 <alphamodeling.contact@gmail.com>
2020-06-17 12:38:15 +0200

    movie fight- Fetch data from omdbapi API

--- javascript/movies/index.js
+++ javascript/movies/index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get(' http://www.omdbapi.com/', {
+    params: {
+      apikey: '6b555dde',
+      i: 'tt2975590',
+    },
+  });
+  console.log(response.data);
+};
+
+fetchData();
