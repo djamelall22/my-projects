5f004f5fac906e669684506139f98a26602f12b6
djamelall22 <alphamodeling.contact@gmail.com>
2020-06-17 21:54:40 +0200

    movies rendering

--- javascript/movies/index.js
+++ javascript/movies/index.js
@@ -6,13 +6,25 @@ const fetchData = async (searchTerm) => {
       s: searchTerm,
     },
   });
-  console.log(response.data);
+
+  return response.data.Search;
 };
 
 //Searching the API on Input Change and delaying search input
 
 const input = document.querySelector('input');
 
-const onInput = (event) => fetchData(event.target.value);
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+  console.log(movies);
+  for (let movie of movies) {
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <img src="${movie.Poster}"/>
+    <h1>${movie.Title}<h1/>
+    `;
+    document.querySelector('#target').appendChild(div);
+  }
+};
 
 input.addEventListener('input', debounce(onInput));
