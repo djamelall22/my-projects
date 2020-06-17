2365eb49e3c32f47f586114ed53ca317c2ee3785
djamelall22 <alphamodeling.contact@gmail.com>
2020-06-17 15:42:46 +0200

    Searching the API on Input Change and delaying search input

--- javascript/movies/index.js
+++ javascript/movies/index.js
@@ -1,11 +1,23 @@
-const fetchData = async () => {
+// Fetch data from omdbapi API
+const fetchData = async (searchTerm) => {
   const response = await axios.get(' http://www.omdbapi.com/', {
     params: {
       apikey: '6b555dde',
-      i: 'tt2975590',
+      s: searchTerm,
     },
   });
   console.log(response.data);
 };
 
-fetchData();
+//Searching the API on Input Change and delaying search input
+
+const input = document.querySelector('input');
+let timeOutId;
+const onInput = (event) => {
+  if (timeOutId) {
+    clearTimeout(timeOutId);
+  }
+  timeOutId = setTimeout(fetchData(event.target.value), 1000);
+};
+
+input.addEventListener('input', onInput);
