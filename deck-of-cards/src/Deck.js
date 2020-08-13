a17bffc0be5398353012ab07697cf5325fd3a7ee
djamelall22 <alphamodeling.contact@gmail.com>
2020-08-13 22:50:10 +0200

    changes

--- deck-of-cards/src/Deck.js
+++ deck-of-cards/src/Deck.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-const API_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle';
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck';
 
 class Deck extends Component {
   constructor(props) {
     super(props);
-    this.state = { deck: null };
-    console.log('inside construction');
+    this.state = { deck: null, drawn: [] };
+    this.getCard = this.getCard.bind(this);
   }
 
   async componentDidMount() {
-    let deck = await axios.get(API_URL);
+    let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
     this.setState({ deck: deck.data });
     console.log(deck.data);
-    console.log('inside did mount');
   }
-  render() {
-    console.log('insid reder');
 
-    return <h1>Card Dealer</h1>;
+  async getCard() {
+    let id = this.state.deck.deck_id;
+    let cardUrl = `${API_BASE_URL}/${id}/draw`;
+    let cardResponse = await axios.get(cardUrl);
+
+    let card = cardResponse.data.cards[0];
+    this.setState((st) => ({
+      drawn: [
+        ...st.drawn,
+        {
+          id: card.code,
+          image: card.image,
+          name: `${card.suit} ${card.value}`,
+        },
+      ],
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Card Dealer</h1>
+        <button onClick={this.getCard}>Get Card!</button>
+      </div>
+    );
   }
 }
 
